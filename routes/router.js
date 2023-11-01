a727133755c4b602c3a995cfce5d3892d0d112fc
Vasi-18 <vasanthkumar1822000@gmail.com>
2023-11-01 16:04:09 +0530

    router last changes

--- routes/router.js
+++ routes/router.js
@@ -169,7 +169,7 @@ router.post("/sendpasswordlink",async(req,res)=>{
                 from:process.env.EMAIL,
                 to:email,
                 subject:"Sending Email For password Reset",
-                text:`This Link Valid For 2 MINUTES https://password-reset-login.netlify.app/api/forgotpassword/${userfind.id}/${setusertoken.verifytoken}`
+                text:`This Link Valid For 2 MINUTES https://password-reset-login.netlify.app/forgotpassword/${userfind.id}/${setusertoken.verifytoken}`
             }
 
             transporter.sendMail(mailOptions,(error,info)=>{
